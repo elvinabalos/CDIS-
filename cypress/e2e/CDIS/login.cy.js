5ca5afb6f8a3ad4f9164ca3d0ff5eeccd3b0fde7
Elvin-bonfire <elvin.abalos.bonfire@gmail.com>
2023-04-17 15:03:50 +0800

    Added password force true on password

--- cypress/e2e/CDIS/login.cy.js
+++ cypress/e2e/CDIS/login.cy.js
@@ -1,18 +1,18 @@
 /// <reference types="cypress" />
 
-describe('The user should be able to login', () => {
+describe('The user should be able to login and logout successfully', () => {
   beforeEach(() => {
     cy.visit('')
   })
 
-  it('should be able to login & logout successfully', () => {
+  it('should be able to input username and password to login and logout', () => {
     // username
     cy.get('.input-group > .form-control').type('superadmin')
 
     // password
     cy.get('.password-input > .form-control').click()
     cy.get('.password-input-addon').should('not.be.checked')
-    cy.get('.password-input > .form-control').type('superadmin031819{enter}')
+    cy.get('.password-input > .form-control').type('superadmin031819{enter}', {force: true})
 
     // user menu
     cy.get('[title="User Menu"]').click().contains('bonfire-administrator')
