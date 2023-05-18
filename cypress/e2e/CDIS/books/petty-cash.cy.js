63b67bab48e0f1e3b11d0af612876602c1e377c2
Elvin-bonfire <elvin.abalos.bonfire@gmail.com>
2023-05-18 17:18:35 +0800

    Added petty cash custodian file with create functionality

---
+++ cypress/e2e/CDIS/books/petty-cash.cy.js
@@ -0,0 +1,54 @@
+import { pettyCash } from "../../../page-objects/petty-cash"
+
+describe('The user should be able to validate clear fields, required fields, duplicates, no data available to show, create, update, delete, and search the currency file', () => {
+  const pettycash = new pettyCash()
+  const username = 'superadmin'
+  const password = 'superadmin031819'
+
+  beforeEach(() => {
+    cy.login(username, password)
+    cy.visit('/petty-cash-custodian-file')
+
+  })
+
+  it.only('Should be able to create petty cash', () => {
+    pettycash.addPettyCash()
+    
+		// cy.contains('EBC STA ANA')
+    // cy.get('.vm-select-dropdown__item.hover', {force: true}).click()
+    // cy.get('.datatable-row--add > :nth-child(5) > .datatable-cell-content > .vm-select > .vm-input > .vm-input__inner')
+    // cy.get('.hover').click()
+  })
+
+ /*
+  it('Should be able to validate clear code, currenct and description fields', () => {
+    currencyfile.validateClearFields()
+  })
+
+  it('Should be able to check validate duplicates petty cash', () => {
+    currencyfile.validateDuplicates()
+  })
+
+  it('Should be able to check validate required fields petty cash', () => {
+    currencyfile.validateRequired()
+  })
+
+  it('Should be able to update petty cash', () => {
+    currencyfile.searchCurrency()  
+    currencyfile.updateCurrency()
+  })
+
+  it('Should be able to search petty cash', () => {
+    currencyfile.searchCurrency()   
+  })
+
+  it('Should be able to delete petty cash', () => {
+    currencyfile.deleteCurrency()  
+  })
+
+  it('Should be able to check if no data available to show the petty cash', () => {
+    currencyfile.checkEmptyResult()
+  })
+
+  */
+})
---
+++ cypress/page-objects/petty-cash.js
@@ -0,0 +1,90 @@
+let code = '100'
+let custodian_name = 'Custodian 1'
+let save_btn = '.row-add'
+let clearBtn = '.datatable-row--add > :nth-child(7)'
+let module_name = 'Petty Cash Custodian'
+let codeField = '.datatable-row--add > :nth-child(2) > .datatable-cell-content > .form-control'
+let custodianField = '.datatable-row--add > :nth-child(4) > .datatable-cell-content > .form-control'
+let statusField = ':nth-child(6) > .form-control'
+let inactiveStatus = 1
+let activeStatus = 0
+let emptyResultModal = '.no-data-available--inline > td'
+let noDataAvailableToShow = 'No data available to show'
+let branchField = '.datatable-row--add > :nth-child(3) > .datatable-cell-content > .vm-select > .vm-input > .vm-input__inner'
+let staAnaBranch = '[style="min-width: 241px; position: absolute; top: 197px; left: 346px; transform-origin: center top; z-index: 2000;"] > .vm-scrollbar > .vm-select-dropdown__wrap > .vm-scrollbar__view > :nth-child(2)'
+let currencyField = '.datatable-row--add > :nth-child(5) > .datatable-cell-content > .vm-select > .vm-input > .vm-input__inner'
+let currencyData = '[style="min-width: 291px; position: absolute; top: 197px; left: 704px; transform-origin: center top; z-index: 2001;"] > .vm-scrollbar > .vm-select-dropdown__wrap > .vm-scrollbar__view > :nth-child(2)'
+
+export class pettyCash {
+	addPettyCash() {
+		cy.get(custodianField).type(custodian_name, {force: true})
+		this.selectBranch()
+		this.selectCurrency()
+		cy.selectStatus(statusField, inactiveStatus)
+		cy.save(save_btn, module_name)
+	}
+
+	selectBranch() {
+		cy.selectData(branchField, staAnaBranch)
+	}
+
+	selectCurrency() {
+		cy.selectData(currencyField, currencyData)
+	}
+
+	searchCurrency() {
+		cy.search(currency)
+	}
+
+	updateCurrency() {
+		cy.update(module_name)
+	}
+
+	deleteCurrency() {
+		this.searchCurrency()
+		cy.delete(module_name)
+	}
+
+	checkEmptyResult() {
+		cy.validateEmptyResult(emptyResultModal, noDataAvailableToShow)
+	}
+
+	validateClearFields() {
+		cy.inputField(codeField, code)
+		cy.inputField(currencyField, currency)
+		cy.inputField(descriptionField, description)
+		cy.get(clearBtn, {force: true}).click()
+		cy.get(codeField)
+		  .should('be.empty')
+		cy.get(currencyField)
+		  .should('be.empty')
+		cy.get(descriptionField)
+		  .should('be.empty')
+	}
+
+	validateDuplicates() {
+		// Validation for duplications
+		cy.inputField(codeField, code)
+		cy.inputField(currencyField, currency)
+		cy.inputField(descriptionField, description)
+		// Save button
+		cy.get(`${save_btn}`).click()
+		cy.get('.datatable-row--add > :nth-child(2) > .datatable-cell-content > .error-message')
+			.should('have.text', `Code has already been taken.`)
+		cy.get('.datatable-row--add > :nth-child(3) > .datatable-cell-content > .error-message')
+			.should('have.text', `Currency has already been taken.`)
+		cy.get('.datatable-row--add > :nth-child(4) > .datatable-cell-content > .error-message')
+			.should('have.text', 'Description has already been taken.')
+	}
+
+	validateRequired() {
+		cy.reload()
+		cy.get(`${save_btn}`).click()
+		cy.get('.datatable-row--add > :nth-child(2) > .datatable-cell-content > .error-message')
+		  .should('have.text', 'Code is required.')
+		cy.get('.datatable-row--add > :nth-child(3) > .datatable-cell-content > .error-message')
+	      .should('have.text', 'Currency is required.')
+		cy.get('.datatable-row--add > :nth-child(4) > .datatable-cell-content > .error-message')
+	      .should('have.text', 'Description is required.')
+	}
+}
